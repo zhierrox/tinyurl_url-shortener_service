4e2f6e7b28cb44b06141c6d87e9062689464e908
zhierrox <xun@Mac.local>
2017-10-26 15:57:48 -0700

    add Docker Redis Nginx

---
+++ app/services/urlService.js
@@ -0,0 +1,121 @@
+var UrlModel = require("../models/urlModel");
+var redis = require("redis");
+var host = process.env.REDIS_PORT_6379_TCP_ADDR;
+var port = process.env.REDIS_PORT_6379_TCP_PORT;
+
+var redisClient = redis.createClient(port, host);
+
+var encode = [];
+var genCharArray = function (charA, charZ) {
+	var arr = [];
+	var i = charA.charCodeAt(0);
+	var j = charZ.charCodeAt(0);
+
+	for (; i <= j; i++) {
+		//unicode to string
+		arr.push(String.fromCharCode(i)); 
+	}
+	return arr;
+};
+
+encode = encode.concat(genCharArray("A", "Z"));
+encode = encode.concat(genCharArray("a", "z"));
+encode = encode.concat(genCharArray("0", "9"));
+
+var getShortUrl = function (longUrl, callback) {
+	if (longUrl.indexOf("http") === -1) {
+		longUrl = "http://" + longUrl;
+	}
+
+	redisClient.get(longUrl, function(err, shortUrl) {
+		if (shortUrl) {
+			console.log("Byebye mongo!");
+			callback({
+				shortUrl: shortUrl,
+				longUrl: longUrl
+			});
+		} else {
+			UrlModel.findOne({longUrl: longUrl}, function(err, data) {
+				if (data) {
+					callback(data);
+					redisClient.set(data.shortUrl, data.longUrl);
+					redisClient.set(data.longUrl, data.shortUrl);
+				} else {
+					generateShortUrl(function (shortUrl) {
+						var url = new UrlModel({
+							shortUrl: shortUrl,
+							longUrl: longUrl
+						});
+						url.save();
+						redisClient.set(shortUrl, longUrl);
+						redisClient.set(longUrl, shortUrl);
+						callback(url);
+					});
+				}
+			});
+		}
+	});
+};
+
+var generateShortUrl = function(callback) {
+	//Object.keys to get hash map size
+	UrlModel.count({}, function(err, data) {
+		callback(convertTo62(data));
+	});
+};	
+
+var convertTo62 = function (num) {
+	var result = "";
+	//includes num == 0
+	do {
+		result = encode[num % 62] + result;
+		//floor is left integer for both positive and negative number
+		num = Math.floor(num / 62);
+	} while (num);
+
+	return result;
+};
+
+var getLongUrl = function(shortUrl, callback) {
+	redisClient.get(shortUrl, function(err, longUrl) {
+		if (longUrl) {
+			callback({
+				shortUrl: shortUrl,
+				longUrl: longUrl
+			});
+		} else {
+			UrlModel.findOne({shortUrl: shortUrl}, function(err, data) {
+				callback(data);
+				if (data) {
+					redisClient.set(data.shortUrl, data.longUrl);
+					redisClient.set(data.longUrl, data.shortUrl);
+				}
+			});
+		}
+	});
+};
+ 
+module.exports = {
+	getShortUrl: getShortUrl,
+	getLongUrl: getLongUrl
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
